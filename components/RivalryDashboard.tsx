e0a01e8f6dad7d4e89011227ff3e44ec285dea3c
Kaiya Wu <kaiya@wirelessprv-10-193-36-99.near.illinois.edu>
2026-03-23 22:13:48 -0500

    fix supabase import path

--- components/RivalryDashboard.tsx
+++ components/RivalryDashboard.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useRouter, useParams } from "next/navigation";
-import { supabase } from "../../lib/supabase";
+import { supabase } from "../lib/supabase";
 import {
   ArrowLeft,
   Swords,
